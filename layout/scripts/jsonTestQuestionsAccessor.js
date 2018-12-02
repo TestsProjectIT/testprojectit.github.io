904aed1155f314a4ac45babb003bd456ba4dd5c6
Repin Stas <stason.repin@yandex.ru>
2018-12-03 00:15:47 +0300

    TestingAgain

--- layout/scripts/jsonTestQuestionsAccessor.js
+++ layout/scripts/jsonTestQuestionsAccessor.js
@@ -1,12 +1,27 @@
-$.ajax({
-    url: 'https://testsprojectit.github.io/data/test.json',
-    dataType: 'json',
-    type: 'get',
-    cache: false,
-    success: function(data){
-        $(data.testID).each(function(index, value)
-                           {
-            console.log(value);
-        });
-    }
-});
+function fillTestsContent()
+{
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200)
+        {
+            //document.getElementById("questions").innerHTML = xhttp.responseText;
+            var response = JSON.parse(xhttp.responseText);
+            var question = response.questions;
+            var output = '';
+            for(var i = 0; i < question.length; i++)
+            {
+                output += '<p><b> Вопрос номер '+ i +'</b></p>'; 
+                var ansewers = question[i].ansewers;
+                for(var j=0; j < ansewers.length; j++)
+                    {
+                        var curAnsewer = ansewers[j];
+                        output += '<p><input type="radio" name="' + question[i].questionID +'" value="' + curAnsewer.score + '"> ' + curAnsewer.ansewerText +'</p>';              
+                    }          
+            }
+    document.getElementById('questions').innerHTML = output;
+                        }
+    };
+
+    xhttp.open("GET", "https://testsprojectit.github.io/data/test.json", true);
+    xhttp.send(); 
+}
