0326b740be5e420c90171c886e9cc79673af1ff1
Repin Stas <stason.repin@yandex.ru>
2018-12-03 03:09:08 +0300

    Update jsonTestQuestionsAccessor.js

--- layout/scripts/jsonTestQuestionsAccessor.js
+++ layout/scripts/jsonTestQuestionsAccessor.js
@@ -79,8 +79,8 @@ function checkAnswers()
             var res = results[indexOfMaxNumber];
             console.log(res);
             
-            output += '<p><b>'+result.resultName + '</b></p>';
-            output += '<p>'+result.resultText + '</p>';
+            output += '<p id="resultName"><b>'+ res.resultName + '</b></p>';
+            output += '<p id="resultText">'+ res.resultText + '</p>';
             output += '<form id="SecondForm">';
             output += '<p><b>Введите Ваше имя для сохранения результата </b></p>';
             output += '<input type="text" id="name" name="name" required minlength="4" maxlength="16">';
@@ -94,13 +94,66 @@ function checkAnswers()
     xhttp.send(); 
 }
 
+function getResults()
+{
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200)
+        {
+            var results = JSON.parse(xhttp.responseText);
+            var users = results.Users;
+            
+            var output = '<p><b>Результаты</b></p>';
+            if(users.length == 0)
+                return;
+            
+            for(var i = 0; i < users.length; i++)
+            {
+                output += '<p><b>Имя пользователя:'+ users[i].name +'</b></p>'; 
+                output += '<p><b>Результат:'+ users[i].result +'</b></p>'; 
+                output += '<br>'
+            }
+            document.getElementById('questions').innerHTML = output;
+        }
+    };
+
+    xhttp.open("GET", "https://testsprojectit.github.io/data/results.json", true);
+    xhttp.send();
+}
+
 function sendInfo()
 {
     var myForm = document.getElementById('SecondForm');
+    var resultName = document.getElementById('resultName');    
     var textBoxes = myForm.getElementsByTagName('input');
     var textBox = textBoxes[0];
     if(textBox.value == "")
         return;
+    
+    var newObj = 
+        {
+            name: textBox.value,
+            result: resultName
+        };
+    
+    var newObject = JSON.stringify(newObj);
+    console.log(newObject);
+    var fs = require('fs');
+    fs.readFile('https://testsprojectit.github.io/data/results.json', 'utf8', function readFileCallback(err, content){
+        if(err){
+            console.log(err);
+        }
+        else
+        {
+            var parseJson = JSON.parse(content);
+            parseJson.Users.push(newObj);
+            var json = JSON.stringify(parseJson);
+            fs.writeFile('https://testsprojectit.github.io/data/results.json', json, 'utf-8', callback);
+        }
+    });
+    
     alert("done");
+    
+    getResults();
     
 }
